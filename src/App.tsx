19ee2f6b9fe114182b248173e169f3eacad681c2
Agostinho Cury <agostinho.cnetto@gmail.com>
2026-04-01 16:13:41 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { Hero } from "./sections/Hero";
 import { Planos } from "./sections/Planos";
 import { Solucoes } from "./sections/Solucoes";
 
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
 function App() {
   return (
     <>
